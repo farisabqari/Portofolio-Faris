319b4006eb39b18605d7ff0c85444da5c96692ef
farisabqari <127277279+farisabqari@users.noreply.github.com>
2025-01-11 19:59:13 +0800

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/farisabqari.github.io/', // Ganti dengan nama repository GitHub Anda
   plugins: [react()],
 })
